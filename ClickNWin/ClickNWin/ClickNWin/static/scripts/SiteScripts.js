1ddf897eb43a4208b2caa5fc1f024a5f8387b401
geoffo5 <c00184861@itcarlow.ie>
2016-12-14 20:02:47 +0000

    screens

--- ClickNWin/ClickNWin/ClickNWin/static/scripts/SiteScripts.js
+++ ClickNWin/ClickNWin/ClickNWin/static/scripts/SiteScripts.js
@@ -5,12 +5,10 @@
     var sPass1 = String(pass1);
     var sPass2 = String(pass2);
 
-
-    alert(pass1 + "\n" + pass2);
     if(pass1 != pass2)
     {
-        alert("no match")
-        //document.getElementById("cpassword").innerHTML = "Passwords do not match";
+        document.getElementById("passMatch1").innerText = "Passwords do not match";
+        document.getElementById("passMatch2").innerText = "Passwords do not match";
         return false;
     }
     
@@ -18,4 +16,14 @@
         
     }    
     
+}
+
+function getBalance()
+{
+    var bal = document.getElementById("balance").innerText;
+    bal = parseFloat(bal);
+    bal += 1;
+    bal = bal.toFixed(2);
+    bal = bal.toString();
+    document.getElementById("balance").innerText = bal;
 }
